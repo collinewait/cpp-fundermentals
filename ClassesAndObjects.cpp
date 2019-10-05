a1d72d8e95fd067dc69a60ce6de02d6d9b4b784d
collinewait <collinewait17@gmail.com>
2019-10-05 19:36:21 +0300

    classes and objects

---
+++ ClassesAndObjects.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Person.h"
+int main()
+{
+    Person p1;
+    Person p2;
+    std::string name = p1.getName();
+
+    // int i = p1.arbitrarynumber; can't access private variable
+    std::cout << "name is " << name << std::endl;
+
+    return 0; 
+}
---
+++ Person.h
@@ -0,0 +1,10 @@
+#include <string>
+class Person
+{
+    private:
+        std::string firstname;
+        std::string lastname;
+        int arbitrarynumber;
+    public:
+        std::string getName();
+};
