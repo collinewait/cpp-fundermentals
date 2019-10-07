1682d5300717a95112ce1ac2ec81723ab0ad732c
collinewait <collinewait17@gmail.com>
2019-10-07 04:54:43 +0300

    write an operator overload

--- ClassesAndObjects.cpp
+++ ClassesAndObjects.cpp
@@ -13,13 +13,26 @@ int main()
         Tweeter t1("Wait", "col", 66754, "@someone");
         string name2 = t1.GetName();
     }
+    Person p2("Collino", "Walker", 123);
     cout << "after innermost block" << endl;
     cout << "p1: " << p1.GetName() << " " << p1.GetNumber() << endl;
     p1.SetNumber(678);
     cout << "p1: " << p1.GetName() << " " << p1.GetNumber() << endl;
 
-    // int i = p1.arbitrarynumber; can't access private variable
-    // cout << "name is " << name << endl;
+    cout << "p1 is ";
+    if(!(p1 < p2))
+        cout << "not ";
+    cout << "less than p2" << endl;
+
+    cout << "p1 is ";
+    if(!(p1 < 300))
+        cout << "not ";
+    cout << "less than 300" << endl;
+
+    cout << "300 is ";
+    if(!(300 < p1))
+        cout << "not ";
+    cout << "less than p1" << endl;
 
     Status s = Pending;
     cout << "status is " << s << endl;
--- Person.h
+++ Person.h
@@ -14,4 +14,7 @@ class Person
         std::string GetName() const;
         int GetNumber() const {return arbitrarynumber;}
         void SetNumber(int number) {arbitrarynumber = number;}
+        bool operator<(Person const& p) const;
+        bool operator<(int i) const; // for things like p1 < 300
 };
+bool operator<(int i, Person const& p); // for things like 300 < p1
